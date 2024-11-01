7381b4967ffc0f216fe9645034a770c8e36a55bb
Pranav Kesavarapu <pranavkesavarapu@gmail.com>
2024-11-02 00:42:17 +0530

    feat: rank the recipes

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,77 @@
-use serde::Deserialize;
-use std::fs;
+use serde::{Deserialize, Deserializer};
+use std::{collections::HashSet, fs};
 
-#[derive(Deserialize, Debug)]
-pub struct Ingredients {
-    name: String,
-    unit_of_measure: String,
-    category: String,
+fn vec_string_from_string<'de, D>(deserializer: D) -> Result<Vec<String>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let keywords = String::deserialize(deserializer)?;
+    match keywords.len() {
+        0 => Ok(vec![]),
+        _ => Ok(keywords.split(",").map(|s| s.trim().to_string()).collect()),
+    }
 }
 
-#[derive(Deserialize, Debug)]
-pub struct AvailableIngredients {
-    name: String,
-}
-
-#[derive(Deserialize, Debug)]
-pub struct Recipes {
+#[derive(Deserialize, Debug, Clone)]
+pub struct Recipe {
     recipe: String,
     instructions: String,
-    ingredients: String,
+    #[serde(deserialize_with = "vec_string_from_string")]
+    ingredients: Vec<String>,
 }
 
-fn initialize() {
-    let ingredients = csv::Reader::from_path("data/ingredients.csv")
-        .unwrap()
-        .deserialize()
-        .collect::<Result<Vec<Ingredients>, _>>()
-        .unwrap();
+#[derive(Debug)]
+pub struct RankedRecipe {
+    recipe: Recipe,
+    rank: usize,
+}
+
+fn initialize() -> (Vec<String>, Vec<Recipe>) {
     let available_ingredients = csv::Reader::from_path("data/available_ingredients.csv")
         .unwrap()
         .deserialize()
-        .collect::<Result<Vec<AvailableIngredients>, _>>()
+        .collect::<Result<Vec<String>, _>>()
         .unwrap();
     let recipes = csv::Reader::from_path("data/recipes.csv")
         .unwrap()
         .deserialize()
-        .collect::<Result<Vec<Recipes>, _>>()
+        .collect::<Result<Vec<Recipe>, _>>()
         .unwrap();
 
-    println!("Ingredients: {ingredients:#?}");
     println!("AvailableIngredients: {available_ingredients:#?}");
     println!("Recipes: {recipes:#?}");
+
+    (available_ingredients, recipes)
+}
+
+fn rank_recipes_by_available_ingredients(
+    available_ingredients: Vec<String>,
+    recipes: Vec<Recipe>,
+) -> Vec<RankedRecipe> {
+    let mut ranked_recipes: Vec<RankedRecipe> = Vec::new();
+
+    recipes.iter().for_each(|recipe| {
+        let required_ingredients: HashSet<_> = recipe.ingredients.iter().collect();
+        let available_count = available_ingredients
+            .iter()
+            .filter(|ingredient| required_ingredients.contains(ingredient))
+            .count();
+        ranked_recipes.push(RankedRecipe {
+            recipe: recipe.clone(),
+            rank: available_count,
+        });
+    });
+
+    ranked_recipes
 }
 
 fn main() {
-    initialize();
+    let (available_ingredients, recipes) = initialize();
+
+    let mut ranked_recipes = rank_recipes_by_available_ingredients(available_ingredients, recipes);
+    ranked_recipes.sort_by_key(|ranked_recipe| ranked_recipe.rank);
+    ranked_recipes.reverse();
+    
+    println!("{ranked_recipes:#?}");
+    
 }
